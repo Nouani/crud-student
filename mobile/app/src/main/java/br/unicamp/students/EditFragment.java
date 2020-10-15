e7efb9687d6fc7cf766d1baf634343b270245b35
Nouani <nouanigabriel@hotmail.com>
2020-10-15 10:38:39 -0300

    H:/Arquivos de Programas/Git/Git/mobile => navegação

---
+++ mobile/app/src/main/java/br/unicamp/students/EditFragment.java
@@ -0,0 +1,18 @@
+package br.unicamp.students;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class EditFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_edit, container, false);
+    }
+}
---
+++ mobile/app/src/main/java/br/unicamp/students/MainActivity.java
@@ -0,0 +1,46 @@
+package br.unicamp.students;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+
+public class MainActivity extends AppCompatActivity {
+    ChipNavigationBar nav;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.nav = (ChipNavigationBar) findViewById(R.id.nav);
+        nav.setItemSelected(R.id.search, true);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new SearchFragment()).commit();
+
+        nav.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int i) {
+                Fragment selectedFragment = null;
+
+                switch(i) {
+                    case R.id.search:
+                        selectedFragment = new SearchFragment();
+                        break;
+                    case R.id.create:
+                        selectedFragment = new CreateFragment();
+                        break;
+                    case R.id.edit:
+                        selectedFragment = new EditFragment();
+                        break;
+                    case R.id.delete:
+                        selectedFragment = new DeleteFragment();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, selectedFragment).commit();
+            }
+        });
+    }
+}
